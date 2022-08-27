8d4c994df828c79c96ae77e6f8525f2bf3846e0e
Roberto Baez <roberto.baez@glic-solutions.com>
2022-08-27 19:56:33 -0300

    Se ordena codigo UserService

--- src/main/java/com/juancho/coin/service/UserService.java
+++ src/main/java/com/juancho/coin/service/UserService.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.stream.Stream;
 
-import org.mapstruct.factory.Mappers;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
@@ -28,9 +27,11 @@ import com.juancho.coin.mappers.CoinMapper;
 import com.juancho.coin.mappers.UserMapper;
 import com.juancho.coin.repository.UserRepository;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
+@RequiredArgsConstructor
 @Slf4j
 @CacheConfig(cacheNames = "userCache")
 public class UserService {
@@ -41,23 +42,17 @@ public class UserService {
 
    private final CoinService coinService;
 
-   private final UserMapper mapper = Mappers.getMapper(UserMapper.class);
+   private final UserMapper userMapper;
 
-   private final CoinMapper mapperCoin = Mappers.getMapper(CoinMapper.class);
-
-
-   public UserService(UserRepository userRepository, CoinService coinService) {
-      this.userRepository = userRepository;
-      this.coinService = coinService;
-   }
+   private final CoinMapper coinMapper;
 
    public UserDto create(UserDto userDto) {
       if (userRepository.existsByUserName(userDto.getUserName())) {
          throw new UserUniqueUserNameException(userDto.getUserName());
       }
-      User user = mapper.fromDto(userDto);
+      User user = userMapper.fromDto(userDto);
       addCoin(userDto, user);
-      return mapper.toDto(userRepository.save(user));
+      return userMapper.toDto(userRepository.save(user));
    }
 
    public UserDto update(UserDto userDto) {
@@ -65,16 +60,16 @@ public class UserService {
       if (userRepository.existsByUserNameAndIdNot(userDto.getUserName(), userDto.getId())) {
          throw new UserUniqueUserNameException(userDto.getUserName());
       }
-      user = mapper.mergeEntity(userDto, user);
+      user = userMapper.mergeEntity(userDto, user);
       addCoin(userDto, user);
-      return mapper.toDto(userRepository.save(user));
+      return userMapper.toDto(userRepository.save(user));
    }
 
    private void addCoin(UserDto userDto, User user) {
       if (!CollectionUtils.isEmpty(userDto.getCoinSet())) {
          Set<Coin> coinSet = new HashSet<>();
          for (CoinDto coin : userDto.getCoinSet()) {
-            coinSet.add(mapperCoin.fromDto(coinService.findById(coin.getId())));
+            coinSet.add(coinMapper.fromDto(coinService.findById(coin.getId())));
          }
          user.setCoinSet(coinSet);
       }
@@ -84,19 +79,19 @@ public class UserService {
       log.info("trying to delete user {}", l);
       User user = userRepository.findById(l).orElseThrow(() -> new UserNotFoundException(l));
       userRepository.delete(user);
-      return mapper.toDto(user);
+      return userMapper.toDto(user);
    }
 
    @Cacheable
    public List<UserDto> findAll() {
       log.info("finding all users");
-      return mapper.toList(userRepository.findAll());
+      return userMapper.toList(userRepository.findAll());
    }
 
    public UserDto findById(Long id) {
       log.info("finding user with id: {}", id);
       User user = userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
-      return mapper.toDto(user);
+      return userMapper.toDto(user);
    }
 
    public UserDto findByUserName(String userName) {
@@ -105,7 +100,7 @@ public class UserService {
          throw new UserPreConditionException();
       }
       User user = userRepository.findByUserName(userName).orElseThrow(() -> new UserNotFoundByUserNameException(userName));
-      return mapper.toDto(user);
+      return userMapper.toDto(user);
    }
 
    public List<UserCoinDto> findAllCoins(Long id) {
